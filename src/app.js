b4edbfa81668a658f386c09d4f59aeefa4b133f5
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-06-07 16:44:28 -0600

    Lesson 6 through to 15 min remaining

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data.weather[0].description);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
 
+let apiKey = "4d0850e5170aa01c3c0597d0577f945a";
+let city = "New York";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+console.log(apiURL);
+
+axios.get(apiURL).then(displayTemperature);
