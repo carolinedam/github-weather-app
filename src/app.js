3d0a77fdb750eabaedcba30e67efde117dfb5864
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-06-12 12:11:37 -0600

    Added JS template / finished lesson 4

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}: ${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/04d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">18°</span>
+
+                  <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   console.log(response.data.weather[0].description);
   let temperatureElement = document.querySelector("#temperature");
@@ -76,7 +104,7 @@ function displayCelsiusTemperature(event) {
 }
 
 search("New York");
-
+displayForecast();
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
