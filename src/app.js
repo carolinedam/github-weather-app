2f2c4d456b3519f4bf758c415d66d3a994067101
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-06-13 10:19:45 -0600

    Connected forecast API / finished lesson 5

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}: ${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thurs", "Fri", "Sat", "Sun"];
@@ -49,6 +50,15 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "4d0850e5170aa01c3c0597d0577f945a";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   console.log(response.data.weather[0].description);
   let temperatureElement = document.querySelector("#temperature");
@@ -72,6 +82,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -104,7 +116,7 @@ function displayCelsiusTemperature(event) {
 }
 
 search("New York");
-displayForecast();
+
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
